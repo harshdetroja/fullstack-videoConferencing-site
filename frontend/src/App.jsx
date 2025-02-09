1945442248c6672b5ac3238afb8db29560dcd5c6
Harsh Detroja <harshdetroja2001@gmail.com>
2025-02-09 13:42:19 +0530

    Authentication done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,18 @@ import "./App.css";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import LandingPage from "./pages/landing";
 import Authentication from "./pages/authentication";
+import { AuthProvider } from "./contexts/AuthContext";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Routes>
-          <Route path="" element={<LandingPage />} />
-          <Route path="/auth" element={<Authentication />} />
-        </Routes>
+        <AuthProvider>
+          <Routes>
+            <Route path="" element={<LandingPage />} />
+            <Route path="/auth" element={<Authentication />} />
+          </Routes>
+        </AuthProvider>
       </Router>
     </div>
   );
--- frontend/src/contexts/AuthContext.jsx
+++ frontend/src/contexts/AuthContext.jsx
@@ -1,8 +1,57 @@
 import axios from "axios";
-import { createContext } from "react";
+import httpStatus from "http-status";
+import { createContext, useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const AuthContext = createContext({});
 
 const client = axios.create({
-  baseURL: "http://localhost:8080/api/v1/",
+  baseURL: "http://localhost:8080/api/v1/users",
 });
+
+export const AuthProvider = ({ children }) => {
+  const authContext = useContext(AuthContext);
+
+  const [userData, setUserData] = useState(authContext);
+
+  const handleRegister = async (name, username, password) => {
+    try {
+      let request = await client.post("/register", {
+        name: name,
+        username: username,
+        password: password,
+      });
+
+      if (request.status === httpStatus.CREATED) {
+        return request.data.message;
+      }
+    } catch (err) {
+      throw err;
+    }
+  };
+
+  const handleLogin = async (username, password) => {
+    try {
+      let request = await client.post("/login", {
+        username: username,
+        password: password,
+      });
+      if (request.status === httpStatus.OK) {
+        localStorage.setItem("token", request.data.token);
+        router("/home");
+      }
+    } catch (err) {
+      throw err;
+    }
+  };
+
+  const router = useNavigate();
+
+  const data = {
+    userData,
+    setUserData,
+    handleRegister,
+    handleLogin,
+  };
+  return <AuthContext.Provider value={data}>{children}</AuthContext.Provider>;
+};
